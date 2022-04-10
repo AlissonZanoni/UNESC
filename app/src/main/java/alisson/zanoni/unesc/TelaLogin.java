eeeaa830e3ec205da4c109c9c627ac1a8e64cfb1
Alisson Zanoni <alisson.zanonii@gmail.com>
2022-04-10 20:32:58 -0300

    Implementando opção de cadastro na tela de login, e ajustando layout

--- app/src/main/java/alisson/zanoni/unesc/TelaLogin.java
+++ app/src/main/java/alisson/zanoni/unesc/TelaLogin.java
@@ -16,7 +16,7 @@ import java.util.Locale;
 public class TelaLogin extends AppCompatActivity {
 
     private EditText edtMatricula, edtSenha;
-    private Button btnEntrar;
+    private Button btnEntrar, btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,45 +26,26 @@ public class TelaLogin extends AppCompatActivity {
         Context context = getApplicationContext();
         int duration = Toast.LENGTH_SHORT;
         btnEntrar = findViewById(R.id.btnEntrar);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
 
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-                edtMatricula = findViewById(R.id.edittextMatricula);
-                edtSenha = findViewById(R.id.edittextSenha);
-
-                String matricula = edtMatricula.getText().toString().trim();
-                String senha = edtSenha.getText().toString().trim();
-
-
-                if(matricula.isEmpty()){
-                    CharSequence text = "Informe o número da Matricula";
-                    Toast toast = Toast.makeText(context,text,duration);
-                    toast.show();
-                }
-                else if (senha.isEmpty()){
-                    CharSequence text = "Informe a senha";
-                    Toast toast =Toast.makeText(context,text,duration);
-                    toast.show();
-                }
-                else if (matricula.equals("123") && senha.equals("qwe123")){
-                    CharSequence text = "Logado com sucesso";
-                    Toast toast =Toast.makeText(context,text,duration);
-                    toast.show();
-                    Intent trocartela = new Intent(TelaLogin.this,TelaHome.class);
-                    startActivity(trocartela);
-                    finish();
-                }
-                else{
-                    CharSequence text = "Matricula ou Senha invalidos";
-                    Toast toast =Toast.makeText(context,text,duration);
-                    toast.show();
-                }
+                trocarDeTela(TelaHome.class);
             }
         });
 
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                trocarDeTela(TelaCadastro.class);
+            }
+        });
+    }
 
+    public void trocarDeTela(Class classname){
+        Intent trocartela = new Intent(TelaLogin.this,classname);
+        startActivity(trocartela);
+        finish();
     }
 }
